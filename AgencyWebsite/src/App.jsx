98f01bc2448d3aa3ad57b4ef555b6fe8346d56cc
AYESHA-AJ <ayeshaaj25@gmail.com>
2024-05-16 23:23:36 +0500

    Slider for home

--- AgencyWebsite/src/App.jsx
+++ AgencyWebsite/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import './App.css'
+import Home from './components/Home'
 
 function App() {
   
 
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <Home/>
     </>
   )
 }
